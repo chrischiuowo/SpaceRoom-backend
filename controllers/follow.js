44d7045c71276261102042d81809e648b21d65d2
Chris <81079621+chrischiu30@users.noreply.github.com>
2022-08-05 15:40:44 +0800

    Fix - follows query name

--- controllers/follow.js
+++ controllers/follow.js
@@ -9,11 +9,11 @@ const apiMessage = require('../service/apiMessage')
   取得追蹤清單成功 GET
 */
 const getFollowsList = catchAsync(async (req, res, next) => {
-  const { target_id } = req.query
+  const { target_user_id } = req.query
 
-  if (!target_id) return next(appError(apiMessage.FIELD_FAILED, next))
+  if (!target_user_id) return next(appError(apiMessage.FIELD_FAILED, next))
 
-  const data = await User.find({ _id: target_id })
+  const data = await User.find({ _id: target_user_id })
     .populate({
       path: 'followings.user',
       select: 'name avatar createdAt'
@@ -40,30 +40,30 @@ const getFollowsList = catchAsync(async (req, res, next) => {
   按讚貼文 與 取消讚貼文 PATCH
 */
 const toggleFollows = catchAsync(async (req, res, next) => {
-  const { follow_id, follow_mode } = req.query
+  const { target_user_id, follow_mode } = req.query
   const user_id = req.user_id
   const follow_toggle = follow_mode === 'follow'
   let data
 
-  if (!follow_id || !follow_mode) { return next(appError(apiMessage.FIELD_FAILED, next)) }
+  if (!target_user_id || !follow_mode) { return next(appError(apiMessage.FIELD_FAILED, next)) }
 
   // 追蹤
   if (follow_toggle) {
     data = await User.findOneAndUpdate(
       {
         _id: user_id,
-        'followings.user': { $ne: follow_id }
+        'followings.user': { $ne: target_user_id }
       },
       {
         $addToSet: {
-          followings: { user: follow_id }
+          followings: { user: target_user_id }
         }
       },
       { new: true }
     )
     await User.findOneAndUpdate(
       {
-        _id: follow_id,
+        _id: target_user_id,
         'followers.user': { $ne: user_id }
       },
       {
@@ -77,13 +77,13 @@ const toggleFollows = catchAsync(async (req, res, next) => {
       { _id: user_id },
       {
         $pull: {
-          followings: { user: follow_id }
+          followings: { user: target_user_id }
         }
       },
       { new: true }
     )
     await User.findOneAndUpdate(
-      { _id: follow_id },
+      { _id: target_user_id },
       {
         $pull: {
           followers: { user: user_id }
